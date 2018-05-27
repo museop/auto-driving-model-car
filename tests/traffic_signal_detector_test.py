121af6751082753a9bd6d2774728e313d5b71f81
museop <kms920806@gmail.com>
2018-05-27 20:50:07 +0900

    update tests

--- tests/traffic_signal_detector_test.py
+++ tests/traffic_signal_detector_test.py
@@ -14,15 +14,21 @@ if __name__ == '__main__':
     while camera.is_opened():
         frame = camera.capture_frame()
         frame = camera.calibrate(frame)
-        can_go = traffic_signal_detector.can_go_forward(frame)
-        print(can_go)
-        cv2.imshow('frame', frame)
-        #  stop_signs = traffic_signal_detector.detect_stop_signs(frame)
-        #  traffic_lights = traffic_signal_detector.detect_traffic_lights(frame)
-        #  cv2.imshow('stop_signs', stop_signs)
-        #  cv2.imshow('traffic_lights', traffic_lights)
 
-        key = cv2.waitKey(10) & 0xFF
+        #  can_go = traffic_signal_detector.can_go_forward(frame)
+        #  print(can_go)
+        #  cv2.imshow('frame', frame)
+        stop_signs = traffic_signal_detector.detect_stop_signs(frame)
+        traffic_lights = traffic_signal_detector.detect_traffic_lights(frame)
+        cv2.imshow('frame', frame)
+
+        if len(stop_signs) > 0:
+            print('Stop signs: ' +  str(stop_signs))
+
+        if len(traffic_lights) > 0:
+            print('Traffic lights: ' + str(traffic_lights))
+
+        key = cv2.waitKey(30) & 0xFF
         if key == ord('q'):
             cv2.destroyAllWindows()
             break
